9bbe46490edffa81cf0620e60cd18a21242a16ed
Amakalu Vitalis <107583430+MerVitz@users.noreply.github.com>
2023-02-22 06:32:48 +0300

    Add files via upload
    Cool Code!
    Brrrrruuh!

---
+++ index.js
@@ -0,0 +1,25 @@
+//Truthy values i Javascript
+
+// Falsy Vaues in Javascript
+// 0
+// ""
+// null ----> How me as a develper signinlizes emptiness/ Signalisez emptiness
+// NaN ----> Not A Number
+// undefined ---> How javascript signalizs emptiness
+
+const credit = 12
+if (credit) {
+    console.log("lets play")
+}else {
+    console.log
+}
+
+let currentViewer = null
+currentViewer = ["Jan", "Nick"]
+ 
+if (currentViewer) {
+    // wil noity if there are viewes in the array of currentViewer
+    console.log("We have viewers")
+}
+
+// the undfined
---
+++ undefined.js
@@ -0,0 +1,19 @@
+//Truthy values i Javascript
+
+// Falsy Vaues in Javascript
+// 0
+// ""
+// null ----> How me as a develper signinlizes emptiness/ Signalisez emptiness
+// NaN ----> Not A Number
+// undefined ---> How javascript signalizs emptiness
+
+let currentViewers = {}
+console.log(currentViewers.randomKey)// its empty
+
+let currentViewer = []
+console.log(currentViewer[5])
+
+
+
+
+
